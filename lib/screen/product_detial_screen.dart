f949235ab93662e25738185176cef951732d87a9
boysad <157202335+kaoheng1515@users.noreply.github.com>
2025-02-22 10:51:54 +0700

    git commit home screen

--- lib/screen/product_detial_screen.dart
+++ lib/screen/product_detial_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_coffee_app/widgets/bottom_nav_bar.dart';
+// import 'package:flutter_coffee_app/widgets/bottom_nav_bar.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({super.key});
